33fdb6ffa1197f753bd44b933547fb8be537ed08
tlk <jeremieboutoille@gmail.com>
2013-04-01 11:34:55 +0200

    Parse header and add some constants

--- pymacho/MachOHeader.py
+++ pymacho/MachOHeader.py
@@ -18,10 +18,35 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from struct import unpack
+from pymacho.Constants import *
 
 class MachOHeader(object):
     """
     Represent a Mach-O Header
+    
+    struct mach_header
+    {
+        uint32_t magic;
+        cpu_type_t cputype;
+        cpu_subtype_t cpusubtype;
+        uint32_t filetype;
+        uint32_t ncmds;
+        uint32_t sizeofcmds;
+        uint32_t flags;
+    };
+    
+    struct mach_header_64
+    {
+        uint32_t magic;
+        cpu_type_t cputype;
+        cpu_subtype_t cpusubtype;
+        uint32_t filetype;
+        uint32_t ncmds;
+        uint32_t sizeofcmds;
+        uint32_t flags;
+        uint32_t reserved;
+    };
+    
     """
     
     def __init__(self, macho_file):
@@ -29,5 +54,11 @@ class MachOHeader(object):
         
     def parse_header(self, macho_file):
         assert macho_file.tell() == 0
-        magic, cputype, cpusubtype, filetype, ncmds, sizeofcmds, flags = unpack("<IIIIIII", macho_file.read(4*7))
+        self.magic, self.cputype, self.cpusubtype, self.filetype = unpack("<IIII", macho_file.read(4*4))
+        self.ncmds, self.sizeofcmds, self.flags = unpack('<III', macho_file.read(4*3))
+        if self.is_64() is True:
+            unpack('<I', macho_file.read(4)) #  uint32_t reserved on mac_header_64
+        
+    def is_64(self):
+        return (self.magic == MH_MAGIC_64 or self.magic == MH_CIGAM_64)
         
