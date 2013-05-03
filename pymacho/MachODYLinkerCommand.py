a6a28a7f6ef1efa9579c9d9a4dbb3f3b73c5117d
tlk <jeremieboutoille@gmail.com>
2013-05-03 19:25:39 +0200

    MachODYLinkerCommand: More logic parsing

--- pymacho/MachODYLinkerCommand.py
+++ pymacho/MachODYLinkerCommand.py
@@ -5,6 +5,7 @@ from struct import unpack
 
 class MachODYLinkerCommand(object):
 
+    offset = 0
     path = ""
 
     def __init__(self, macho_file=None):
@@ -12,10 +13,12 @@ class MachODYLinkerCommand(object):
             self.parse(macho_file)
 
     def parse(self, macho_file):
-        # get the offset
+        # get the cmdsize
         macho_file.seek(-4, 1)
-        cmdsize = unpack('<II', macho_file.read(8))[0]
+        cmdsize = unpack('<I', macho_file.read(4))[0]
+        # get the offset
+        self.offset = unpack('<I', macho_file.read(4))[0]
         # get the string
-        strlen = cmdsize - 12
+        strlen = cmdsize - self.offset
         extract = "<%s" % ('s'*strlen)
         self.path = "".join(char if char != "\x00" else "" for char in unpack(extract, macho_file.read(strlen)))
